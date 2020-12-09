07019218f7cb39084a3202f5bd0e465b6aa6b332
Kurmanbek <kumakaraev01@gmail.com>
2020-12-09 09:59:51 -0800

    header + bottom navigations work

--- App.js
+++ App.js
@@ -2,16 +2,10 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import React from "react";
-import { View, Text } from 'react-native';
-
 import {FontAwesome5} from "@expo/vector-icons";
-import { LinearGradient } from 'expo-linear-gradient';
 
 //custom imports
-import Dashboard from "./src/screens/Dashboard";
-import Report from "./src/screens/Report";
-import Credits from "./src/screens/Credits";
-import Profile from "./src/screens/Profile";
+import {DashboardHeader, ReportHeader, CreditsHeader, ProfileHeader} from "./src/navigation/headerStackNavi";
 
 
 const Tab = createMaterialBottomTabNavigator();
@@ -19,22 +13,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Dashboard" labeled={false} activeColor="#30beb2" barStyle={{height: 65, backgroundColor: "white"}}>
-        <Tab.Screen name="Dashboard" component={Dashboard} options={{
+        <Tab.Screen name="Dashboard" component={DashboardHeader} options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="home" light color={color} size={20} />
           ), }
         } />
-        <Tab.Screen name="Report" component={Report} options={{
+        <Tab.Screen name="Report" component={ReportHeader} options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="chart-line" color={color} size={20} />
           ), }
         } />
-        <Tab.Screen name="Credits" component={Credits} options={{
+        <Tab.Screen name="Credits" component={CreditsHeader} options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="credit-card" color={color} size={20} />
           ), }
         } />
-        <Tab.Screen name="Profile" component={Profile} options={{
+        <Tab.Screen name="Profile" component={ProfileHeader} options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="user-alt" color={color} size={20} />
           ), }
---
+++ src/navigation/headerStackNavi.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {createStackNavigator} from "@react-navigation/stack";
+import Dashboard from "../screens/Dashboard";
+import Report from "../screens/Report";
+import Credits from "../screens/Credits";
+import Profile from "../screens/Profile";
+import { LinearGradient } from 'expo-linear-gradient';
+const Stack = createStackNavigator();
+
+//gradient color header
+
+
+const DashboardHeader = () => {
+    return (
+        <Stack.Navigator screenOptions={{
+            headerBackground: () => (
+                <LinearGradient
+                  colors={[ '#17f9f2', '#b0f9a9']}
+                  style={{ flex: 1 }}
+                  start={{x: 0, y: 0}}
+                  end={{x: 1, y: 0}}
+                />
+            ),
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            }
+            }}>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+    );
+}
+
+const ReportHeader = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Report" component={Report} />
+        </Stack.Navigator>
+    );
+}
+const CreditsHeader = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Credits" component={Credits} />
+        </Stack.Navigator>
+    );
+}
+const ProfileHeader = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+    );
+}
+
+export {DashboardHeader, ReportHeader, CreditsHeader, ProfileHeader}
