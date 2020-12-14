886a6f4aa6f64dc6e60aedb65abc6e006e867669
Kurmanbek <kumakaraev01@gmail.com>
2020-12-14 11:00:41 -0800

    not finished with design

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text,Image, FlatList, Dimensions, View } from 'react-native';
 import {FontAwesome5} from "@expo/vector-icons";
 import { LinearGradient } from 'expo-linear-gradient';
-import { ScrollView } from 'react-native-gesture-handler';
 
 
 const { width, height } = Dimensions.get('window')
@@ -12,13 +10,19 @@ const DATA = [{
     number: "2234 2213 4222 2444",
     cardholder: "Kumas Karaev",
     expire: "01/24",
-    balance: 3211.00,
+    balance: "3,250.00"
 },
 {
     number: "2234 4222 2444 4444",
     cardholder: "KOKODKE",
     expire: "02/21",
-    balance: 22221.00,
+    balance: "55,250.00",
+},
+{
+    number: "1111 4222 2444 4444",
+    cardholder: "dawdadw",
+    expire: "02/21",
+    balance: "5,250.00",
 },
 ]
 const FancyHeader = () => { 
@@ -47,40 +51,69 @@ const FancyHeader = () => {
     );
 }
 
+const Constants = {
+    marginTop: 10,
+    marginBottom: 12,
+    largeFont: 20,
+    mediumFont: 17 ,
+    smallerFont: 10
+}
+
+
 const styles = StyleSheet.create({ 
+    Container: {},
     CardBody: {
-       flex: 1,
-       width: width - 20,
-       height: height / 4,
-       backgroundColor: 'white',
-       margin: 10,
-       padding: 15,
-       borderRadius: 20,
+        backgroundColor: 'white',
+        padding: 20,
+        width: width-40,
+        borderRadius: 15
     },
     Text_CardTypeImage: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
-    CurrentBalance: {
+    Title: {
+        fontSize: 17,
+        marginBottom: Constants.marginBottom,
+        fontWeight: '500',
+        color: 'grey'
+    },
+    CurrentBalanceContainer: {
         flexDirection: 'row',
+        marginBottom: Constants.marginBottom
     },
     USD_Sign: {
-        backgroundColor: "#17f9f2",
-        borderRadius: 9,
-        padding: '2%',
+        backgroundColor: '#17f9f2',
+        padding: 8,
+        borderRadius: 10
+    },
+    USD_Sign_Text: {
+        color: "white",
+        fontWeight: '700'
     },
     Balance: {
-        padding: '1%',
+        fontWeight: '700',
         fontSize: 20,
-        fontWeight: 'bold'
+        margin: 2,
+        marginLeft: 5
+    },
+    CardNumberContainer: {
+        marginBottom: Constants.marginBottom
     },
     CardNumber: {
-        fontSize: 18
+        fontSize: 20
     },
     Cardholder_ExpiryDate: {
         flexDirection: 'row',
         justifyContent: 'space-between'
     }
+
+
+
+
+
+
 });
 
 const CarouselCreditCard = ({item}) => {
@@ -90,9 +123,9 @@ const CarouselCreditCard = ({item}) => {
             <Text style={styles.Title}>Current Balance</Text>
             <FontAwesome5 name="cc-visa" light size={30} />
         </View>
-        <View style={styles.CurrentBalance}>
+        <View style={styles.CurrentBalanceContainer}>
             <View style={styles.USD_Sign}>
-                <Text>USD</Text>
+                <Text style={styles.USD_Sign_Text}>USD</Text>
             </View>
             <Text style={styles.Balance}>{item.balance}</Text>
         </View>
@@ -100,8 +133,8 @@ const CarouselCreditCard = ({item}) => {
             <Text style={styles.CardNumber}>{item.number}</Text>
         </View>
         <View style={styles.Cardholder_ExpiryDate}>
-            <Text>{item.cardholder}</Text>
-            <Text>{item.expire}</Text>
+            <Text style={styles.cardholder}>{item.cardholder}</Text>
+            <Text style={styles.expire}>{item.expire}</Text>
         </View>
     </View>
     )
@@ -109,10 +142,11 @@ const CarouselCreditCard = ({item}) => {
 class CreditCardComponents extends Component {
     render() {
         return(
-            <View>
+            <View styles={styles.Container}>
                 <FlatList data ={DATA} keyExtractor={(item, index) => 'key' + index} 
                     horizontal scrollEnabled snapToAlignment='center' pagingEnabled 
                     scrollEventThrottle={16}
+                    contentContainerStyle={{paddingHorizontal: 10}}
                     decelerationRate={"fast"}
                     showsHorizontalScrollIndicator={false}
                     renderItem={({item}) => {
